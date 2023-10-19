a0c625459c78d6fd14728b9106366dfa1441523c
Huzaifa <huzaifakashif2003@gmail.com>
2023-10-19 15:53:02 +0500

    all taxes addedmif else conditions for taxes

--- taxes.c
+++ taxes.c
@@ -39,6 +39,31 @@ else if (agi>=19900 && agi<81050)
   tax=agi;
   tax=((tax+1990)/100)*12;
 }
+else if (agi>=81050 && agi<172750)
+{
+  tax=agi;
+  tax=((tax+9328)/100)*22;
+}
+else if(agi>=172750 && agi<329850)
+{
+  tax=agi;
+  tax=((tax+29502)/100)*24;
+}
+else if(agi>=329850 && agi<418850)
+{
+  tax=agi;
+  tax=((tax+67206)/100)*32;
+}
+else if(agi>418850 && agi<628300)
+{
+  tax=agi;
+  tax=((tax+95686)/100)*35;
+}
+else if(agi>=628301)
+{
+  tax=agi;
+  tax=((tax+168933.50)/100)*37;
+}
 
   printf("AGI:          $%10.2f\n", agi);
   printf("Tax:          $%10.2f\n", tax);
